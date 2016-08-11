b8723cf28cd2465f3c2a21ffa7d2d70f2eff4186
ravi-project <ravi_d@yahoo.com>
2016-08-11 10:24:01 -0400

    Update and rename apachebootstrapscript.sh to apache-puppetbootstrapscript.sh

--- apache-puppetbootstrapscript.sh
+++ apache-puppetbootstrapscript.sh
@@ -10,3 +10,8 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www -type f -exec chmod 0664 {} +
 echo "<hello-world>" > /var/www/html/hello.php
+apt-get install -y puppet
+echo "project.puppetagent1.com" > /etc/hostname
+echo "master-ip-address project.puppetmaster.com" > /etc/hosts
+echo "server=project.puppetmaster.com" > /etc/puppet/puppet.conf
+
